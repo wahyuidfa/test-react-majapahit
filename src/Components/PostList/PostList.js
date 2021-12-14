660ab7dd8008d8e7d47311833ff1542d4439ad06
Wahyu Idfa Daru <wahyuidfadaru3@gmail.com>
2021-12-14 17:12:12 +0700

    update post list

--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import useStyles from "./styles"
 
-function PostList() {
+function PostList({post}) {
+    const styles = useStyles()
     return (
-        <div>
-            
-        </div>
+       <Card elevation={3} classname={styles.root}>
+           <CardContent>
+               <div className={styles.cardContent}>
+                   <Typography variant="p" fontSize={"18px"}>
+                       {post.title}
+                   </Typography>
+               </div>
+           </CardContent>
+       </Card>
     );
 }
 
---
+++ src/Components/PostList/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles(() => ({
+    root : {
+        maxWidth: "100%",
+        // justifyContent: "center"
+        boxShadow:"none",
+      
+    },
+    cardContent: {
+        display:"flex",
+        justifyContent: "space-between"
+    }
+}))
